83d771982f90b867bad8418b733903ae21b6a85a
Arshveer Singh Gahir <arshveersinghgahir@gmail.com>
2021-05-24 20:29:00 -0400

    Update data.py
    Working on Data

--- data.py
+++ data.py
@@ -2,3 +2,13 @@ import pandas as pd
 data  =  pd.read_csv("appl.csv")
 print(data.head())
 data["Date"]=pd.to_datetime(data.Date)
+data.head()
+
+data[‘Open’] = data[‘Open’].replace({"\$":’’}, 
+regex=True).astype(float)
+
+'''
+data[‘High’] = data[‘High’].replace({"\$": ‘’}, regex=True).astype(float)
+data[‘Close/Last’] = data[‘Close/Last’].replace({"\$": ‘’}, regex=True).astype(float)
+data[‘Low’] = data[‘Low’].replace({"\$": ‘’}, regex=True).astype(float)
+'''
