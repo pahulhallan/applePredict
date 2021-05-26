5066903651a38f5e2f3b181ef5ebb81be337d81b
pahulhallan <pahulhallan@gmail.com>
2021-05-26 18:41:48 -0400

    rejoined files

--- data.py
+++ data.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
 data  =  pd.read_csv("appl.csv")
 data["Date"]=pd.to_datetime(data.Date)
 
@@ -24,3 +29,51 @@ data_to_test.to_csv("test_data.csv")
 
 
 print(data.head())
+
+#model
+training_set = data_to_train.iloc[:, 3:4].values
+real_stock_price = data_to_test.iloc[:, 3:4].values
+
+sc = MinMaxScaler(feature_range=(0, 1))
+training_data_scaled = sc.fit_transform(training_set)
+# The fit method is calculating the mean and variance of each of the features present in our data. The transform method is transforming all the features using the respective mean and variance. Now, we want scaling to be applied to our test data too and at the same time do not want to be biased with our model
+
+X_train = []
+y_train = []
+for i in range(60, 1000):
+    X_train.append(training_data_scaled[i - 60:i, 0])
+    y_train.append(training_data_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Building Model:
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.LSTM(units=50, return_sequences=True,
+                               input_shape=(X_train.shape[1], 1)))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.LSTM(units=50, return_sequences=True))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.LSTM(units=50, return_sequences=True))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.LSTM(units=50))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(units=1))
+model.summary()
+#We use Dropout layers to avoid Overfitting problems, and besides that, we use the parameter “return_sequences” to determine if the layer will return a sequence compatible with a LSTM. We use “return_sequences=True” when we have a LSTM layer after!
+
+
+modelo = model.fit(X_train, y_train, epochs=100, batch_size=32)
+
+
+####################Predict
+dataset_total = pd.concat([data_to_train["Open"], data_to_test["Open"]], axis=0)
+inputs = dataset_total[len(dataset_total) - len(data_to_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 259):
+   X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+predicted_stock_price = model.predict(X_test)
+predicted_stock_price = model.predict(X_test)
