f666fc1bd44ad31d0965cd1fcb8bee6896d52170
Arshveer Singh Gahir <arshveersinghgahir@gmail.com>
2021-05-26 18:36:05 -0400

    Data Model Ready

--- data.py
+++ data.py
@@ -1,14 +1,26 @@
 import pandas as pd
 data  =  pd.read_csv("appl.csv")
-print(data.head())
 data["Date"]=pd.to_datetime(data.Date)
-data.head()
 
-data[‘Open’] = data[‘Open’].replace({"\$":’’}, 
-regex=True).astype(float)
 
-'''
-data[‘High’] = data[‘High’].replace({"\$": ‘’}, regex=True).astype(float)
-data[‘Close/Last’] = data[‘Close/Last’].replace({"\$": ‘’}, regex=True).astype(float)
-data[‘Low’] = data[‘Low’].replace({"\$": ‘’}, regex=True).astype(float)
-'''
+#Formatting Data w/out $$
+
+data["Open"] = data["Open"].str.replace("\$", "",regex = True).astype(float)
+data["High"] = data["High"].str.replace("\$", "",regex = True).astype(float)
+data["Close/Last"] = data["Close/Last"].str.replace("\$", "",regex = True).astype(float)
+data["Low"] = data["Low"].str.replace("\$", "",regex = True).astype(float)
+
+#Formatting Columns
+
+data.rename(columns= {" Close/Last" : "Close/Last", " Volume" : "Volume",
+ " Open" : "Open", " High" : "High", " Low" : "Low"}, inplace = True) 
+
+#Split into train and test:
+data_to_train = data[:1000]
+data_to_test = data[1000:]
+
+data_to_train.to_csv("train_data.csv")
+data_to_test.to_csv("test_data.csv")
+
+
+print(data.head())
